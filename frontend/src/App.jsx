2a49e364dce5f93dd30102faeb1a569525b6f29c
Roy Louis <thorlouis17@gmail.com>
2024-07-14 19:15:32 +0530

    frontend initial setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,28 @@
 import './App.css'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import SignUp from './pages/auth/signUp';
+import Home from './components/home';
+import Login from './pages/auth/login';
+import Sidebar from './components/sideBar';
+import RightPanel from './components/rightPanel';
+import Notification from './components/notification';
+import Profile from './components/Profie.jsx';
 
 function App() {
 
+  const authUser = true;
+
   return (
-    <div className='font-bold text-red-800'>
-     hfhrdhs
+    <div className='flex max-w-6xl mx-auto'>
+      <Sidebar/>
+<Routes>
+				<Route path='/' element={authUser ? <Home /> : <Navigate to='/' />} />
+				<Route path='/login' element={!authUser ? <Login /> : <Navigate to='/login' />} />
+				<Route path='/signup' element={!authUser ? <SignUp /> : <Navigate to='/signup' />} />
+				<Route path='/notifications' element={authUser ? <Notification /> : <Navigate to='/' />} />
+				<Route path='/profile/:username' element={authUser ? <Profile /> : <Navigate to='/' />} />
+			</Routes>
+      <RightPanel/>
     </div>
   )
 }
---
+++ frontend/src/components/post/posts.jsx
@@ -0,0 +1,28 @@
+import { POSTS } from "../../utils/data";
+import PostSkeleton from "../skeleton/postSkeleton";
+import Post from "./post";
+
+const Posts = () => {
+	const isLoading = false;
+
+	return (
+		<>
+			{isLoading && (
+				<div className='flex flex-col justify-center'>
+					<PostSkeleton />
+					<PostSkeleton />
+					<PostSkeleton />
+				</div>
+			)}
+			{!isLoading && POSTS?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
+			{!isLoading && POSTS && (
+				<div>
+					{POSTS.map((post) => (
+						<Post key={post._id} post={post} />
+					))}
+				</div>
+			)}
+		</>
+	);
+};
+export default Posts;
